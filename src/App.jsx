28cfe84187cdd26aa03c7622b0daea4ea9ce50dc
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2025-04-17 16:12:56 +0100

    create nav bar with search bar (search not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import PokemonList from "./pages/PokemonList"
-import PokemonDetail from "./pages/PokemonDetail"
+import { Route, Routes } from "react-router-dom";
+import PokemonList from "./pages/PokemonList";
+import PokemonDetail from "./pages/PokemonDetail";
+import Navbar from "./components/NavBar";
+// import NavBar from "";
 
 function App() {
-
   return (
     <>
-
-    <Routes >
-
-    <Route path="/" element={<PokemonList />} />
-
-    <Route path='/pokemon/:name' element={<PokemonDetail />} />
-
-    </Routes>
-  
-  </>
-  )
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
+        
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
